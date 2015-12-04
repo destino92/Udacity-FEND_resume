781cf51cf41fbeacee6254e0dd826fa35385428f
destino92 <destinodello@gmail.com>
2015-12-04 19:42:09 +0300

    add education section

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -14,7 +14,7 @@ var bio = {
 		"bioPic": "images/me.jpg",
 		"welcomeMessage": "Hoye I'm Destino, a Front End Developper and this is my online resume",
 		"skills": ["html5","css3","twitter bootstrap","grunt","JavaScript","jQuery","Git","github","photoshop"]
-	};
+	}
 
 var formattedName = HTMLheaderName.replace("%data%",bio["name"]);
 var formattedRole = HTMLheaderRole.replace("%data%",bio["role"]);
@@ -56,8 +56,56 @@ var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.location);
 var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.description);
 
 $("#workExperience").append(HTMLworkStart);
-$(".work-entry").append(formattedWorkEmployer);
-$(".work-entry").append(formattedWorkTitle);
+$(".work-entry").append(formattedWorkEmployer + formattedWorkTitle);
 $(".work-entry").append(formattedWorkDates);
 $(".work-entry").append(formattedWorkLocation);
 $(".work-entry").append(formattedWorkDescription);
+
+//education section
+var education = {
+	"schools": [
+		{
+			"name" : "Eastern Mediteranean University",
+			"degree" : "Bsc",
+			"dates" : 2011,
+			"location" : "Gasimagusa, TRNC",
+			"major" : "Computer Engineering"
+		}
+	],
+	"onlineCourses": [
+		{
+			"Title" : "JavaScript Programming: Learn by Making a Mobile Game",
+			"School" : "Udemy",
+			"Dates" : 2015,
+			"URL" : "https://www.udemy.com/certificate/UC-BHKYRZV2/"
+		}
+	]
+};
+
+var formattedschoolName = HTMLschoolName.replace("%data%", education.schoolName);
+var formattedschoolDegree = HTMLschoolDegree.replace("%data%", education.schoolDegree);
+var formattedschoolDates = HTMLschoolDates.replace("%data%", education.schoolDates);
+var formattedschoolLocation = HTMLschoolLocation.replace("%data%", education.schoolLocation);
+var formattedschoolMajor = HTMLschoolMajor.replace("%data%", education.schoolMajor); 
+
+$("#education").append(HTMLschoolStart);
+$(".education-entry").append(formattedschoolName + formattedschoolDegree);
+$(".education-entry").append();
+$(".education-entry").append(formattedschoolDates);
+$(".education-entry").append(formattedschoolLocation);
+$(".education-entry").append(formattedschoolMajor);
+
+education.onlineTitle = "JavaScript Programming: Learn by Making a Mobile Game";
+education.onlineSchool = "Udemy";
+education.onlineDates = "2015";
+education.onlineURL = "https://www.udemy.com/certificate/UC-BHKYRZV2/";
+
+var formattedonlineTitle = HTMLonlineTitle.replace("%data%", education.onlineTitle);
+var formattedonlineSchool = HTMLonlineSchool.replace("%data%", education.onlineSchool);
+var formattedonlineDates = HTMLonlineDates.replace("%data%", education.onlineDates);
+var formattedonlineURL = HTMLonlineURL.replace("%data%", education.onlineURL);
+
+$(".education-entry").append(HTMLonlineClasses);
+$(".education-entry").append(formattedonlineTitle + formattedonlineSchool);
+$(".education-entry").append(formattedonlineDates);
+$(".education-entry").append(formattedonlineURL);
